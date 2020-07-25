1e9ebcaa799b32e92069096daa3b7cb5d106b743
srinivasamanohar <67685998+srinivasamanohar@users.noreply.github.com>
2020-07-25 20:33:44 +0530

    Create DataTypes.js

---
+++ Week1/Day5/DataTypes.js
@@ -0,0 +1,8 @@
+console.log(typeof(undefined));     // Undefined
+console.log(typeof(true));          // Boolean
+console.log(typeof(null));          // Object
+console.log(typeof([]));            // Object
+console.log(typeof({}));            // Object  
+console.log(typeof(1));             // Number
+console.log(typeof(1.1));           // Number
+console.log(typeof("1.1"));         // String
