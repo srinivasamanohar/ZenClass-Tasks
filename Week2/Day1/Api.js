6fc91c3170a97c7786ca27840488050a63811d7c
srinivasamanohar <67685998+srinivasamanohar@users.noreply.github.com>
2020-07-28 23:52:02 +0530

    Update Api.js

--- Week2/Day1/Api.js
+++ Week2/Day1/Api.js
@@ -5,10 +5,8 @@ let url= 'https://restcountries.eu/rest/v2/all'
 
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET',url, true);
-//request.setRequestHeader('Access-Control-Allow-Origin', 'www.google.com');
 request.send();
 request.onload = function() {
-  // Begin accessing JSON data here
 var data = JSON.parse(this.response)
 console.log(data);
 }
