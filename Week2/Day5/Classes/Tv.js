6ff79b5aacbba3801890e47c42a6183a4dd2b058
srinivasamanohar <67685998+srinivasamanohar@users.noreply.github.com>
2020-08-04 12:47:09 +0530

    Update Tv.js

--- Week2/Day5/Classes/Tv.js
+++ Week2/Day5/Classes/Tv.js
@@ -1,31 +1,73 @@
 class TV{
-    constructor(...args){
-        [this.brand, this.channel = 1, this.price, this.inches, this.OnOFFStatus, this.volume = 50] = args;
+    constructor(...args)
+    {
+        [this.brand,  this.price, this.inches, this.OnOFFStatus,this.channel = 1, this.volume = 50] = args;
     }
 
-    increaseVolume(){
-        if(this.volume < 100) this.volume++;
+    increaseVolume()
+    {
+        if(this.volume < 100) 
+        {
+            this.volume++;
+        }
     }
 
-    decreaseVolume(){
-        if(this.volume > 0) this.volume--;
+    decreaseVolume()
+    {
+        if(this.volume > 0) 
+        {
+            this.volume--;
+        }
     }
 
-    reset(){
+    reset()
+    {
         this.volume = 50;
-        this.channel = 1
+        this.channel = 1;
     }
 
-    status(){
+    status()
+    {
         return this.brand + ' at channel ' + this.channel + ', volume' + this.volume;
     }
 
-    setChannel(){
-        if(value > 50) this.channel = value;
+    setChannel()
+    {
+        if(value > 50) 
+        {
+            this.channel = value;
+        }
     }
 }
 
-class LED_TV{
+class LED extends TV 
+{
+    constructor(screenThickness, energyUse, lifeSpan, refreshRate, ...args)
+    {
+        super(args);
+        this.screenThickness = screenThickness;
+        this.energyUse = energyUse;
+        this.lifeSpan = lifeSpan;
+        this.refreshRate = refreshRate;
+    }
+
+    viewingAngel()
+    { 
+        return this.viewingAngel;
+    }
+    
+    backLight()
+    { 
+        return this.backLight;
+    }
+    display()
+    { 
+        return this.energyUse + this.lifeSpan + this.refreshRate;
+    }
+}
+
+class Plasma extends TV
+{
     constructor(screenThickness, energyUse, lifeSpan, refreshRate, ...args){
         super(args);
         this.screenThickness = screenThickness;
@@ -34,21 +76,22 @@ class LED_TV{
         this.refreshRate = refreshRate;
     }
 
-    viewingAngel(){ return this.viewingAngel;}
-    backLight(){ return this.backLight;}
-    display(){ return }
-}
-
-class PlasmaTV{
-    constructor(screenThickness, energyUse, lifeSpan, refreshRate, ...args){
-        super(args);
-        this.screenThickness = screenThickness;
-        this.energyUse = energyUse;
-        this.lifeSpan = lifeSpan;
-        this.refreshRate = refreshRate;
+    viewingAngel()
+    { 
+        return this.viewingAngel;
     }
-
-    viewingAngel(){ return this.viewingAngel;}
-    backLight(){ return this.backLight;}
-    display(){ return }
+    backLight()
+    { 
+        return this.backLight;
+    }
+    display()
+    { 
+        return this.energyUse + this.lifeSpan + this.refreshRate;
+    }
 }
+
+let tv = new TV("SONY",50000,50,"On")
+console.log(tv)
+
+let led = new LED(150,5,10,50,brand="INTEX",price=10000,inches=150,OnOffstatus="ON")
+console.log(led)
